b7605c215ae6f888a2361040937b62be0ef0e7a7
Moto Shinriki <m8o8t2o8@gmail.com>
2017-08-09 16:31:18 +0900

    ライブラリインポートをコメントアウト

--- WhiteboardOnline/ApiManager.swift
+++ WhiteboardOnline/ApiManager.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2017年 Moto Shinriki. All rights reserved.
 //
 
-import Alamofire
-import SwiftyJSON
+//import Alamofire
+//import SwiftyJSON
 
 class ApiManager{
 	func Login(id: String, password: String)-> Bool{
