f0c19e9e49741919c72fce8fe21e4f4a27908461
HY_RORRE <hy_rorre@hotmail.com>
2017-08-09 16:16:36 +0900

    Create class ApiManager, Group, Card

---
+++ WhiteboardOnline/Card.swift
@@ -0,0 +1,21 @@
+//
+//  Card.swift
+//  WhiteboardOnline
+//
+//  Created by hyrorre on 2017/08/09.
+//  Copyright © 2017年 Moto Shinriki. All rights reserved.
+//
+
+class Card{
+	var id = 0
+	var commentFront = ""
+	var commentBack = ""
+	var isFront = true
+	var imageId = 0
+	var color = 0
+	var x = 0
+	var y = 0
+	var width = 1
+	var height = 1
+	var creator = 0
+}
---
+++ WhiteboardOnline/Group.swift
@@ -0,0 +1,14 @@
+//
+//  Group.swift
+//  WhiteboardOnline
+//
+//  Created by hyrorre on 2017/08/09.
+//  Copyright © 2017年 Moto Shinriki. All rights reserved.
+//
+
+class Group{
+	var id = 0
+	var name = ""
+	var thumbnail = 0
+	var comment = ""
+}
